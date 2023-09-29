286d75727c0faf470a612932438630963ab5317f
Hemilsoni975 <104961337+Hemilsoni975@users.noreply.github.com>
2023-09-29 11:34:53 +0530

    Add files via upload

---
+++ progress/07_operators.py
@@ -0,0 +1,55 @@
+# opertors in python
+
+# Arithmetic operator
+
+print("15 + 6 : ", 15+6)
+print("15 - 6 : ", 15-6)
+print("15 * 6 : ", 15*6)
+print("15 / 6 : ", 15/6)
+print("15 // 6 : ", 15//6)
+
+# Assignment operator
+
+x = 5
+print(x)
+# x += 7
+# x -= 7
+# x *= 7
+# x /= 7
+x /= 7
+print(x)
+
+# comparision operator
+
+i = 5
+h = 10
+print(i <= h)
+print(i >= h)
+print(i != h) 
+print(i == 5)
+
+# logical operator
+
+a = 50
+b = 30
+print(a and b)
+print(a or b)  
+print(a != b)
+
+# identity operator
+
+w = 10
+v = 15
+print(w is v)
+print(w is not v)
+
+#membership operator
+
+mylist = [1,2,34,5,6,7]
+print(10 not in mylist)
+print(55 in mylist)
+
+# bitwise operator 
+# here it works as boolean 1 means true and 0 means false
+print(0 | 3) # it is true 
+print(0 & 1) # it is false
