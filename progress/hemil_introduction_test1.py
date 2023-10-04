dcf9ccdc5989e2f22f477db5d9977be60fbd6af9
Hemilsoni975 <104961337+Hemilsoni975@users.noreply.github.com>
2023-10-04 12:12:41 +0530

    Update hemil_introduction_test1.py

--- progress/hemil_introduction_test1.py
+++ progress/hemil_introduction_test1.py
@@ -4,12 +4,11 @@
 # output: [z] 0
 
 a = input("Please Enter Your FullName : ")
-
 x = 0
+
 for i in a:
     if i >= "0" and i <= "9":
         x = 1
-
 
 if x == 1:
     print("please print only string")
@@ -17,6 +16,7 @@ else:
     b = input("which letter you want to count?? ")
     c = a.count(b)
     print("repeated times : ", c)
+
 
 # print below pattern
 #  *****
