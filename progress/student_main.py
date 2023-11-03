e34cd6692314a2c6e87675fe7559dad687920ab8
Hemilsoni975 <104961337+Hemilsoni975@users.noreply.github.com>
2023-11-03 14:03:58 +0530

    test4_student_main.py

--- progress/student_main.py
+++ progress/student_main.py
@@ -1,14 +1,14 @@
-# class
-# object
-# abstraction
-# constructor
-# polymorphism
-# encapsulation
-# destructor
+# # class
+# # object
+# # abstraction
+# # constructor
+# # polymorphism
+# # encapsulation
+# # destructor
 
 from test4_student_abstract import wow
 
-
+cs = []
 class SOU(wow):
     def __init__(self, cn):
         super().__init__()
@@ -33,29 +33,34 @@ class College(wow):
     def displayInfo(self):
         print(f"NAME OF DEAN : {self.hod}")
 
-class course(wow):
-    def __init__(self, name, sub_code):
-        self.name = name
-        self.sub_code = sub_code
-        self.course = []
+class course:
+    def __init__(self, course_code, course_name):
+        self.course_code = course_code
+        self.course_name = course_name
 
-    def add_course(self,courses):
-        self.course.append(courses)
 
-    def displayInfo(self):
-        print(f"SELECTED COURSE : {self.name}\nSUBJECT CODE : {self.sub_code}")
-
-class Student():
+class Student(wow):
 
     def __init__(self, sname, enroll, college, sem):
         self.sname = sname
         self.enroll = enroll
         self.college = college
         self.sem = sem
+        self.courses = []
+
+    def sub(self, course):
+        self.courses.append(course)
+
+    def displayCourse(self):
+        if not self.courses:
+            print("NO SUBJECT SELECTED!!!")
+        else:
+            print("SUBJECT :")
+            for course in self.courses:
+                print(f"COURSE CODE : {course.course_code}, COURSE NAME : {course.course_name}")
 
     def displayInfo(self):
-        print(f"NAME OF STUDENT IS {self.sname} WITH ENROLLMENT NO. {self.enroll} STUDIES IN {self.college}\nHE LIKE {self.sem} language")
-
+        print(f"{self.sname} WITH {self.enroll} OF {self.college} HAS SELECTED {cs} SUBJECTS")
 
 class head:
     def __init__(self):
@@ -63,7 +68,7 @@ class head:
         self.__pNum = 9876543210
         self.__pProfession = "TEACHER"
         self.__pSalary = 100000
-        self.__pLecture = ["python", "java"]
+        self.__pLecture = ["PYTHON", "JAVA"]
 
     def __pDisplay(self):
         print(f"""PRINCIPLE NAME : {self.__pName}\nPRINCIPLE NUMBER : {self.__pNum}\nPRINCIPLE PROFESSION : {self.__pProfession}\nPRINCIPLE SALARY : {self.__pSalary}\nPRINCIPLE LECTURE : {self.__pLecture}""")
@@ -95,25 +100,43 @@ sem = input("ENTER SEMESTER : ")
 s = Student(sname, enroll, college, sem)
 
 # COURSE DETAIL
-name = input("ENTER SUBJECT NAME : ")
-sub_code = input("ENTER SUBJECT CODE : ")
-c = course(name, sub_code)
 
 
-# display uni name
-clg_name.displayInfo()
+while True:
+    print("Select an option:")
+    print("1. SELECT SUBJECTS")
+    print("2. FINISH")
 
-# display hod
-a.displayInfo()
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        course_code = input("ENTER SUBJECT CODE : ")
+        course_name = input("ENTER SUBJECT NAME : ")
+
+        c = course(course_code, course_name)
+
+        print(c.course_code)
+        print(c.course_name)
 
 
-# display student details
-s.displayInfo()
+        cs.append(c.course_name)
+        print("Course created successfully!")
 
-# display course
-c.displayInfo()
+    elif choice == "2":
 
-# private method
-h = head()
-print(h.displayPrivateData())
-del h
+        # display universty name
+        clg_name.displayInfo()
+
+        # display dean
+        a.displayInfo()
+
+        # display student details
+        s.displayInfo()
+
+        # private method
+        # h = head()
+        # print(h.displayPrivateData())
+        # del h
+
+        exit()
